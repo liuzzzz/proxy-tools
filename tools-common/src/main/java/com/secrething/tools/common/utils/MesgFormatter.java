0d25e9e413dd1c1f99dbcf6ebccf53e7ce6411b2
zengzeng.liu <zengzeng.liu@gloryholiday.com>
2018-04-23 19:52:24 +0800

    jdk1.7

--- tools-common/src/main/java/com/secrething/tools/common/utils/MesgFormatter.java
+++ tools-common/src/main/java/com/secrething/tools/common/utils/MesgFormatter.java
@@ -10,10 +10,6 @@ public abstract class MesgFormatter {
     private static final String PLACE_HOLDER = "{}";
     static int split = PLACE_HOLDER.length();
 
-    private MesgFormatter() {
-        throw new UnsupportedOperationException("");
-    }
-
     /**
      * 字符串拼接,拼接方式如同slf4j日志的输出方式
      * 例: format("h{}ll{}","e","o") return hello
@@ -23,11 +19,14 @@ public abstract class MesgFormatter {
      * @return
      */
     public static final String format(String pattern, Object... params) {
+        return formatWithHolder(pattern, PLACE_HOLDER, params);
+    }
+    public static final String formatWithHolder(String pattern, String hoder, Object... params) {
         if (params.length < 1 || StringUtils.isBlank(pattern))
             return pattern;
         char[] src = pattern.toCharArray();
         int offset = 0;
-        int idx = pattern.indexOf(PLACE_HOLDER, offset);
+        int idx = pattern.indexOf(hoder, offset);
         if (idx < 0)
             return pattern;
         StringBuilder builder = new StringBuilder();
@@ -37,11 +36,15 @@ public abstract class MesgFormatter {
                 builder.append(src, offset, len);
             builder.append(params[i]);
             offset = idx + split;
-            idx = pattern.indexOf(PLACE_HOLDER, offset);
+            idx = pattern.indexOf(hoder, offset);
         }
         if (offset < src.length) {
             builder.append(src, offset, src.length - offset);
         }
         return builder.toString();
+    }
+
+    public static final void println(String pattern, Object... params) {
+        System.out.println(format(pattern, params));
     }
 }
